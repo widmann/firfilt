d0417bf00bb533c09484f9466b171b2fe9dce7d5
widmann <widmann@uni-leipzig.de>
2008-04-15 15:12:16 +0000

    Version number

--- eegplugin_firfilt.m
+++ eegplugin_firfilt.m
@@ -33,7 +33,7 @@
 
 function vers = eegplugin_firfilt(fig, trystrs, catchstrs)
 
-    vers = 'firfilt1.1';
+    vers = 'firfilt1.2';
     if nargin < 3
         error('eegplugin_firfilt requires 3 arguments');
     end
