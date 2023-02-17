9dcd87d16ce6ab93f060664bfb40ead0cc7edbb1
Arnaud Delorme <arnodelorme@gmail.com>
2023-02-17 09:41:43 -0800

    change version num

--- eegplugin_firfilt.m
+++ eegplugin_firfilt.m
@@ -31,7 +31,7 @@
 
 function vers = eegplugin_firfilt(fig, trystrs, catchstrs)
 
-    vers = 'firfilt2.6.1';
+    vers = 'firfilt2.6';
     if nargin < 3
         error('eegplugin_firfilt requires 3 arguments');
     end
